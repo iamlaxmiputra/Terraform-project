772cc2a4941eaa89f567b1027c5a524d0894f733
iamlaxmiputra <laxmiputra@akdhar.com>
2024-08-20 19:22:18 +0000

    aded s3code

---
+++ terraforms3.tf
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------------------------
+# CREATE THE S3 BUCKET
+# ------------------------------------------------------------------------------
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+}
